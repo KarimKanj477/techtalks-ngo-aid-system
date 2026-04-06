65d84f2fa2217c7cb0b861446a45ebeb728cb707
User <User@ZEIN>
2026-04-07 00:21:56 +0300

    Add project files.

---
+++ NGOSystem/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using NGOSystem.Models;
+using System.Diagnostics;
+
+namespace NGOSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ NGOSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NGOSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
