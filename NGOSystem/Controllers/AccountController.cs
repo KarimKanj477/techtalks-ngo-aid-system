7e3479dd3e33d5098569e7146ef79c49a4ceaab3
KarimKanj477 <karim.kanj05@gmail.com>
2026-04-09 16:00:52 +0300

    KAN-4: add register and login views with AccountController

---
+++ NGOSystem/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NGOSystem.Models;
+
+namespace NGOSystem.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Login");
+            }
+
+            return View(user);
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
+
